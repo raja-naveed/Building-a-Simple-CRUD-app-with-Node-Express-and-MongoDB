ceb77b9121fdefa2d74a5ebd2fbe3227e233af35
“Raja <rajadahri11@gmail.com>
2023-03-03 19:19:06 +0500

    Added Update PUT method through button

--- server.js
+++ server.js
@@ -1,43 +1,65 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'))
-app.use(bodyParser.json())
+app.use(express.static("public"));
+app.use(bodyParser.json());
 // mongodb+srv://rajadahri11:2U42RySiCedejB3c@cluster0.lxwesof.mongodb.net/?retryWrites=true&w=majority
-const url =
-  "mongodb://localhost:27017";
-  const client = new MongoClient(url, { useUnifiedTopology: true });
+// getting local host url
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
-client.connect()
+// connecting to database
+client
+  .connect()
   .then((client) => {
     console.log("Connected to Database");
+    // creating a database
     const db = client.db("raja");
+    // creating a collection in database
     const quotesCollection = db.collection("quotes");
-    app.post('/quotes', (req, res) => {
-      quotesCollection.insertOne(req.body)
+
+    // post data to database
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => console.error(error));
+    });
+    // get data from database
+    app.get("/", (req, res) => {
+      db.collection("quotes")
+        .find()
+        .toArray()
+        .then((results) => {
+          res.render("index.ejs", { quotes: results });
+        })
+        .catch(/* ... */);
+    });
+
+    // update data from database
+    app.put("/quotess", (req, res) => {
+      quotesCollection.findOneAndUpdate(
+        { name: 'Hosh' },
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        }
+      )
         .then(result => {
-          console.log(result)
+          res.json('Success')
         })
         .catch(error => console.error(error))
-    })
-    app.get('/', (req, res) => {
-      db.collection('quotes').find().toArray()
-        .then(results => {
-          res.render('index.ejs', { quotes: results })
-        })
-        .catch(/* ... */)
-       
     });
-    app.put('/quotes', (req, res) => {
-      console.log(req.body)
-      console.log("Hello This Is My Group")
-    })
-  
 
+    // listing on port 3000
     app.listen(3000, function () {
       console.log("listening on 3000");
     });
--- views/public/main.js
+++ views/public/main.js
@@ -2,12 +2,19 @@ let update = document.getElementById("update-button");
 
 function call(){
  console.log("Hello");
- fetch("/quotes", {
-   method: "put",
-   headers: { "Content-Type": "application/json" },
-   body: JSON.stringify({
-     name: "Darth Vader",
-     quote: "I find your lack of faith disturbing.",
-   }),
- });
+//  fetching data from database
+fetch('/quotes', {
+  method: 'put',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    name: 'Darth Vadar',
+    quote: 'I find your lack of faith disturbing.'
+  })
+})
+  .then(res => {
+    if (res.ok) return res.json()
+  })
+  .then(response => {
+  console.log(response);
+  })
 };
